a334da87eea9b04995b485bfd884b7d31f27abd4
James Wallen-Jones <j.wallen.jones@googlemail.com>
2019-12-23 10:12:27 +0000

    Fix missing use statement

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
 
 class AuthController extends ApiController
 {
