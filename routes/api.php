729746b27a575a667dbb135080d39a2ec9eedd66
James Wallen-Jones <j.wallen.jones@googlemail.com>
2020-01-24 09:17:39 +0000

    Add in passport routing, add default route for organisations

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('')->group(function () {
+Route::prefix('organisation')->group(function () {
     Route::get('', 'OrganisationController@listAll');
     Route::post('', 'OrganisationControlller@create');
 });
