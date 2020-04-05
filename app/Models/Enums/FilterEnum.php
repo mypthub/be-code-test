ad029bef2f90adfcf8b54aca9bf3ee7f32e56446
horidhenry <robarnold100@hotmail.com>
2020-04-05 15:58:15 +0100

    filter enum added for organisation lookup

---
+++ app/Models/Enums/FilterEnum.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models\Enums;
+
+use MyCLabs\Enum\Enum;
+
+/**
+ * FilterEnum
+ */
+
+ class FilterEnum extends Enum
+ {
+     const SUBBED = 'subbed';
+     const TRIAL = 'trial';
+     const ALL = 'all';
+ }
+
